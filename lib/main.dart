3f00811c3a2de21d0103696823e5ff10fd8936db
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-03-06 20:01:55 -0600

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -55,25 +55,35 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           Text('Pull to refresh.'),
+          // This can only be used with a vertical scroll view
           RefreshIndicator(
             onRefresh: () async {
+              // This function updates the scrollable contents,
+              // in this case the items in a ListView.
+              // The refresh indicator disappears when
+              // the promise returned by this function completes.
+              addItem();
               addItem();
               addItem();
             },
+            //TODO: Figure out what this changes!
+            triggerMode: RefreshIndicatorTriggerMode.anywhere,
             //TODO: Fix ability to scroll when there are
             //TODO: more items than will fit on the screen.
-            child: ListView.builder(
-              physics: const AlwaysScrollableScrollPhysics(),
-              itemCount: items.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                    title: Text(
-                      items[index],
+            child: Scrollbar(
+              child: ListView.builder(
+                physics: const AlwaysScrollableScrollPhysics(),
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(
+                        items[index],
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ).expanded,
         ],
